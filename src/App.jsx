9bc2ff3abf9f44adf9dea0e2aa15ed1b4d7f8740
sanyam <patilsanyam869@gmail.com>
2023-12-30 12:13:34 +0530

    API Setup & Context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AppContext } from './context/ContextApi'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>youtubeclone</h1>
-  
+    <>
+      <AppContext>
+        <h1>youtubeclone</h1>
+      </AppContext>
+    </>
   )
 }
 
